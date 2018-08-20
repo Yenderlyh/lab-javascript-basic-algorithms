aee622bb5c70e591f6fba0623f400d84c4594bf2
Yenderlyh <40594938+Yenderlyh@users.noreply.github.com>
2018-08-20 23:37:22 +0200

    BCN - Yenderly

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -32,7 +32,7 @@ if (hacker1 > hacker2){
   }else if(hacker1 < hacker2){
     console.log('The navigator goes first definitely')
   }else{
-    console.log('What?! You both got the same name?')
+    console.log('What?! You  both got the same name?')
   }
 // Lorem ipsum generator
 var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
